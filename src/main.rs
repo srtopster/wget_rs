a7f75cd1ce1e2111cf36f16a08a099d25fc2fc06
Topster_ <lixoespamdoadamastor@gmail.com>
2022-04-01 22:28:43 -0300

    Adicionei uma forma de escolher o nome do arquivo antes de baixar. (wget URL NOME_DO_ARQUIVO)

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,36 @@ use std::process;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        println!("Uso: wget URL");
+        println!("Uso: wget URL (filename)");
         process::exit(1)
     }
-    if args[1].starts_with("https://") | args[1].starts_with("http://"){
-        let url = &args[1];
-        let file_name = &args[1].split("/").collect::<Vec<&str>>();
-        let resp = reqwest::get(url).await?;
-        let size = resp.content_length().unwrap() as f64;
-        let mut stream = resp.bytes_stream();
+    if !args[1].starts_with("https://") & !args[1].starts_with("http://"){
+        println!("Url invalida.");
+        process::exit(1)
+    }
+    let url = &args[1];
+    let filename: &str;
+    if args.len() >= 3{
+        filename = &args[2];
+    }
+    else {
+        filename = &args[1].split("/").last().unwrap();
+    }
+    let resp = reqwest::get(url).await?;
+    let size = resp.content_length().unwrap() as f64;
+    let mut stream = resp.bytes_stream();
 
-        println!("[{:.2}Mb]{}",(size/1048576.0),file_name.last().unwrap());
-        let mut file = File::create(file_name.last().unwrap()).unwrap();
-        let mut done: f64 = 0.0;
-        let old = Instant::now();
-        while let Some(item) = stream.next().await {
-            let chunk = item.or(Err(format!("Erro ao baixar")))?;
-            file.write_all(&chunk).unwrap();
-            done += chunk.len() as f64;
-            let perc = ((done/size)*100.0) as i32;
-            print!("\r[{}][{:.2}Mbps]{}%","=".repeat(perc as usize/2)+&" ".repeat(50-perc as usize/2),(done/(old.elapsed().as_secs() as f64))/1048576.0,perc);
-            io::stdout().flush().unwrap()
-        }
+    println!("[{:.2}Mb]{}",(size/1048576.0),filename);
+    let mut file = File::create(filename).unwrap();
+    let mut done: f64 = 0.0;
+    let old = Instant::now();
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Erro ao baixar")))?;
+        file.write_all(&chunk).unwrap();
+        done += chunk.len() as f64;
+        let perc = ((done/size)*100.0) as i32;
+        print!("\r[{}][{:.2}Mbps]{}%","=".repeat(perc as usize/2)+&" ".repeat(50-perc as usize/2),(done/(old.elapsed().as_secs() as f64))/1048576.0,perc);
+        io::stdout().flush().unwrap();
     }
     Ok(())
 }
